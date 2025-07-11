6e6d474633b4f5782e5330fcc9682455996a6813
llanes37 <78422866+llanes37@users.noreply.github.com>
2025-07-11 11:20:28 +0200

    subida

--- juego/script.js
+++ juego/script.js
@@ -7,55 +7,75 @@ class Player {
         this.x = 0
         this.y = 0
 
-        this.playerSize = canvas.width/15
+        this.vx = 0
+        this.vy = 0
+        this.gravity = 0.5
+        this.jumpForce = -10
+        this.onGround = false
 
-        this.isMoving = false
+        this.playerSize = canvas.width / 15
+        this.speed = 2
 
         document.addEventListener("keydown", (e) => {
-            if (!this.isMoving) {
-                this.isMoving = true
-                this.interval = setInterval(() => {
-                    this.move(e)
-                }, 10)
+            if (e.key === "ArrowLeft") this.vx = -this.speed
+            if (e.key === "ArrowRight") this.vx = this.speed
+            if (e.key === " " && this.onGround) {
+                this.vy = this.jumpForce
+                this.onGround = false
             }
         })
-        
-        document.addEventListener("keyup", () => {
-            clearInterval(this.interval)
-            this.isMoving = false
+
+        document.addEventListener("keyup", (e) => {
+            if (e.key === "ArrowLeft" || e.key === "ArrowRight") this.vx = 0
         })
     }
 
     collide(x, y) {
         let pixelColor = ctx.getImageData(x, y, 1, 1).data
-        if (pixelColor[3] == 255) return false
-        else return true
+        return pixelColor[3] !== 255 // true si se puede pasar
     }
 
-    move(e) {
-        switch (e.key) {
-            case "ArrowLeft":
-                if (this.collide(this.x-1, this.y) && this.collide(this.x-1, this.y + this.playerSize - 1)) {
-                    this.x-=2
-                }
-                break
-            case "ArrowRight":
-                if (this.collide(this.x + this.playerSize, this.y) && this.collide(this.x + this.playerSize, this.y + this.playerSize - 1)) {
-                    this.x+=2
-                }
-                break
-            case "ArrowUp":
-                if (this.collide(this.x, this.y-1) && this.collide(this.x + this.playerSize - 1, this.y-1)) {
-                    this.y-=2
-                }
-                break
-            case "ArrowDown":
-                if ((this.collide(this.x, this.y + this.playerSize)) && this.collide(this.x + this.playerSize - 1, this.y + this.playerSize)) {
-                    this.y+=2
-                }
-                break
+    update() {
+        // Gravedad
+        this.vy += this.gravity
+
+        // Movimiento horizontal
+        if (
+            this.vx < 0 &&
+            this.collide(this.x - 1, this.y) &&
+            this.collide(this.x - 1, this.y + this.playerSize - 1)
+        ) {
+            this.x += this.vx
+        } else if (
+            this.vx > 0 &&
+            this.collide(this.x + this.playerSize, this.y) &&
+            this.collide(this.x + this.playerSize, this.y + this.playerSize - 1)
+        ) {
+            this.x += this.vx
+        }
+
+        // Movimiento vertical
+        this.y += this.vy
+
+        // Colisión con suelo (parte inferior del canvas)
+        if (this.y + this.playerSize >= canvas.height) {
+            this.y = canvas.height - this.playerSize
+            this.vy = 0
+            this.onGround = true
+        } else {
+            // Verifica si está tocando suelo (pixel sólido debajo)
+            let belowLeft = ctx.getImageData(this.x + 1, this.y + this.playerSize, 1, 1).data[3]
+            let belowRight = ctx.getImageData(this.x + this.playerSize - 1, this.y + this.playerSize, 1, 1).data[3]
+            if (belowLeft === 255 || belowRight === 255) {
+                this.y = Math.floor(this.y / this.playerSize) * this.playerSize
+                this.vy = 0
+                this.onGround = true
+            } else {
+                this.onGround = false
+            }
         }
     }
+
     draw() {
         ctx.fillRect(this.x, this.y, this.playerSize, this.playerSize)
     }
@@ -69,19 +89,19 @@ function update() {
 
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height)
 
+    player.update()
     player.draw()
-    
+
     requestAnimationFrame(update)
 }
 
 background.onload = () => {
     ctx.imageSmoothingEnabled = false
 
-    // console.log((html.clientWidth / canvas.width))
+    // Zoom comentado
     // if (html.clientWidth < 700) {
     //     canvas.style.zoom = Math.floor(html.clientWidth / canvas.width)
-    // }
-    // else {
+    // } else {
     //     canvas.style.zoom = Math.floor(html.clientWidth / canvas.width) / 2
     // }
 
