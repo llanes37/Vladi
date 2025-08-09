debc02861261dc09f133dd08ee60da4982ef4a05
vnebx <vladyslav.neborak.oficial@gmail.com>
2025-08-09 10:02:44 +0200

    actualizacion backend python

--- flask_project/my_app/__init__.py
+++ flask_project/my_app/__init__.py
@@ -1,11 +1,17 @@
+# my_app/__init__.py
 from flask import Flask
 from config import Config
+from my_app.models import db
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    db.init_app(app)
+
     with app.app_context():
-        from . import routes
+        db.create_all()
+        from .routes import register_routes
+        register_routes(app)  # ← ESTO ES CLAVE
 
     return app
--- flask_project/my_app/routes.py
+++ flask_project/my_app/routes.py
@@ -1,5 +1,45 @@
-from flask import current_app, render_template
+# my_app/routes.py
+from flask import render_template, redirect, url_for, request
+from my_app.models import db, Juego
+from my_app.forms import JuegoForm
 
-@current_app.route('/')
-def index():
-    return render_template('index.html')
+def register_routes(app):
+    @app.route('/')
+    def index():
+        juegos = Juego.query.all()
+        return render_template('index.html', juegos=juegos)
+
+    @app.route('/nuevo', methods=['GET', 'POST'])
+    def nuevo_juego():
+        form = JuegoForm()
+        if form.validate_on_submit():
+            juego = Juego(
+                nombre=form.nombre.data,
+                genero=form.genero.data,
+                plataforma=form.plataforma.data,
+                descripcion=form.descripcion.data
+            )
+            db.session.add(juego)
+            db.session.commit()
+            return redirect(url_for('index'))
+        return render_template('nuevo_juego.html', form=form)
+
+    @app.route('/editar/<int:id>', methods=['GET', 'POST'])
+    def editar_juego(id):
+        juego = Juego.query.get_or_404(id)
+        form = JuegoForm(obj=juego)
+        if form.validate_on_submit():
+            juego.nombre = form.nombre.data
+            juego.genero = form.genero.data
+            juego.plataforma = form.plataforma.data
+            juego.descripcion = form.descripcion.data
+            db.session.commit()
+            return redirect(url_for('index'))
+        return render_template('editar_juego.html', form=form)
+
+    @app.route('/eliminar/<int:id>', methods=['POST'])
+    def eliminar_juego(id):
+        juego = Juego.query.get_or_404(id)
+        db.session.delete(juego)
+        db.session.commit()
+        return redirect(url_for('index'))
